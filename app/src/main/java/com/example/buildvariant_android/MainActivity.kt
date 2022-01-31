582ab14f77564015105eb6c47b93e5bb317f55e0
iamraajkanchan <iamraajkanchan@gmail.com>
2022-01-31 16:45:38 +0530

    MainAcivity is updated

--- app/src/main/java/com/example/buildvariant_android/MainActivity.kt
+++ app/src/main/java/com/example/buildvariant_android/MainActivity.kt
@@ -4,24 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity()
-{
-    companion object
-    {
+class MainActivity : AppCompatActivity() {
+    companion object {
         const val TAG = "MainActivity"
     }
 
-    override fun onCreate(savedInstanceState : Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getBuildVariantDetail()
     }
 
-    private fun getBuildVariantDetail()
-    {
-        val data = BuildConfig.VERSION_NAME
-        Log.d(TAG , data)
+    private fun getBuildVariantDetail() {
+        val versionName = BuildConfig.VERSION_NAME
+        Log.d(TAG, "Version Name: $versionName")
     }
 }
